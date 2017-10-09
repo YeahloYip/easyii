52da24dfb065a28c92735a02cae3b78dd0dc8628
Yeahlo <yeahlo@gmail.com>
2017-10-09 11:44:56 +0800

    message/zh-CN

---
+++ messages/zh-CN/article.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'Edit article' => '修改文章',
+    'Article created' => '文章已创建',
+    'Article updated' => '文章已修改',
+    'Article deleted' => '文章已删除',
+];
---
+++ messages/zh-CN/install.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'Installation' => 'Installation',
+    'Install' => 'Install',
+    'Root password' => 'Root password',
+    'Admin E-mail' => 'Admin E-mail',
+    'Robot E-mail' => 'Robot E-mail',
+    'Auth time' => 'Auth time',
+    'Frontend toolbar position' => 'Frontend toolbar position',
+    'Password to login as root' => 'Password to login as root',
+    'Used as "ReplyTo" in mail messages' => 'Used as "ReplyTo" in mail messages',
+    'Used as "From" in mail messages' => 'Used as "From" in mail messages',
+    'Required for using captcha in forms (guestbook, feedback)' => 'Required for using captcha in forms (guestbook, feedback)',
+    'You easily can get keys on' => 'You easily can get keys on',
+    'ReCaptcha website' => 'ReCaptcha website',
+    'Installation completed' => 'Installation completed',
+    'Installation error' => 'Installation error',
+    'Cannot connect to database. Please configure `{0}`.' => 'Cannot connect to database. Please configure `{0}`.',
+    'EasyiiCMS is already installed. If you want to reinstall easyiiCMS, please drop all tables with prefix `easyii_` from your database manually.' => 'EasyiiCMS is already installed. If you want to reinstall easyiiCMS, please drop all tables with prefix `easyii_` from your database manually.',
+];
